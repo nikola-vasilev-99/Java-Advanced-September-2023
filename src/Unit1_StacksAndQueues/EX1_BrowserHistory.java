97417cd089f44344fb8f25c5247aacb99f2fa17b
Nikola Vasilev <nikola.vasilev99@icloud.com>
2023-09-14 17:15:47 +0300

    summary

---
+++ src/Unit1_StacksAndQueues/EX1_BrowserHistory.java
@@ -0,0 +1,29 @@
+package Unit1_StacksAndQueues;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class EX1_BrowserHistory {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        ArrayDeque <String> urlStack = new ArrayDeque<>();
+        String currentUrl = "";
+        while (!input.equals("Home")) {
+            if (!input.equals("back")) {
+                urlStack.push(input);
+                currentUrl = input;
+                System.out.println(currentUrl);
+            } else {
+                if (urlStack.size() > 1) {
+                    urlStack.pop();
+                    currentUrl = urlStack.peek();
+                    System.out.println(currentUrl);
+                } else {
+                    System.out.println("no previous URLs");
+                }
+            }
+            input = scanner.nextLine();
+        }
+    }
+}
