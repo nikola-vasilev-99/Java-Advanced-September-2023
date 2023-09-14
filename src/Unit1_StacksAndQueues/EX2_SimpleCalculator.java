58c06963b66cfa5770bc7eaa1fc964238291e239
Nikola Vasilev <nikola.vasilev99@icloud.com>
2023-09-14 18:38:02 +0300

    nomessage

---
+++ src/Unit1_StacksAndQueues/EX2_SimpleCalculator.java
@@ -0,0 +1,12 @@
+package Unit1_StacksAndQueues;
+
+import java.util.Scanner;
+
+public class EX2_SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String text = scanner.nextLine();
+
+    }
+}
