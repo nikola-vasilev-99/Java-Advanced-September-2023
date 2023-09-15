b84b6d417520755aeccf4a1bde4169335d168131
Nikola Vasilev <nikola.vasilev99@icloud.com>
2023-09-15 17:10:31 +0300

    exercise

---
+++ src/Unit1_StacksAndQueues/EX5_PrinterQueue.java
@@ -0,0 +1,31 @@
+package Unit1_StacksAndQueues;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class EX5_PrinterQueue {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayDeque <String> printerQueue = new ArrayDeque<>();
+
+        String input = scanner.nextLine();
+        while (!input.equals("print")) {
+            if (!input.equals("cancel")) {
+                printerQueue.offer(input);
+            } else {
+                if (printerQueue.isEmpty()) {
+                    System.out.println("Printer is on standby");
+                } else {
+                    String firstFile = printerQueue.poll();
+                    System.out.println("Canceled " + firstFile);
+                }
+            }
+            input = scanner.nextLine();
+        }
+        while (printerQueue.size() != 0) {
+            String nextFile = printerQueue.poll();
+            System.out.println(nextFile);
+        }
+
+    }
+}
