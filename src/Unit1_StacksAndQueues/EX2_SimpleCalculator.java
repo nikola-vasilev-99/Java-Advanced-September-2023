06145027a5f2f9c9b6e21bbd40d1342e7d572637
Nikola Vasilev <nikola.vasilev99@icloud.com>
2023-09-15 14:03:22 +0300

    summary

--- src/Unit1_StacksAndQueues/EX2_SimpleCalculator.java
+++ src/Unit1_StacksAndQueues/EX2_SimpleCalculator.java
@@ -1,12 +1,33 @@
 package Unit1_StacksAndQueues;
 
+import java.util.ArrayDeque;
 import java.util.Scanner;
 
 public class EX2_SimpleCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String text = scanner.nextLine();
+        String[] array = scanner.nextLine().split("\\s+");
+        ArrayDeque <String> stack = new ArrayDeque<>();
 
+        for (int i = array.length - 1; i >= 0; i--) {
+            stack.push(array[i]);
+        }
+
+        while (stack.size() > 1) {
+            int firstNum = Integer.parseInt(stack.pop());
+            String operator = stack.pop();
+            int secondNum = Integer.parseInt(stack.pop());
+
+            switch (operator) {
+                case "+":
+                    stack.push(String.valueOf(firstNum + secondNum));
+                    break;
+                case "-":
+                    stack.push(String.valueOf(firstNum - secondNum));
+                    break;
+            }
+        }
+        System.out.println(stack.peek());
     }
 }
---
+++ src/Unit1_StacksAndQueues/EX3_DecimalToBinaryConverter.java
@@ -0,0 +1,11 @@
+package Unit1_StacksAndQueues;
+
+import java.util.Scanner;
+
+public class EX3_DecimalToBinaryConverter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
