388ed01ba358b6c5e6a00c0c45a9f551135096e1
Nikola Vasilev <nikola.vasilev99@icloud.com>
2023-09-15 21:30:50 +0300

    grade 100/100

---
+++ src/Unit1_StacksAndQueues/EX7_MathPotato.java
@@ -0,0 +1,11 @@
+package Unit1_StacksAndQueues;
+
+import java.util.Scanner;
+
+public class EX7_MathPotato {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
---
+++ src/Unit1_StacksAndQueues/EX8_BrowserHistoryUpgrade.java
@@ -0,0 +1,38 @@
+package Unit1_StacksAndQueues;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class EX8_BrowserHistoryUpgrade {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        ArrayDeque<String> urlStack = new ArrayDeque<>();
+        ArrayDeque<String> forwardUrl = new ArrayDeque<>();
+        String currentUrl = "";
+        while (!input.equals("Home")) {
+            if (input.equals("back")) {
+                if (urlStack.size() > 1) {
+                    forwardUrl.addFirst(urlStack.pop());
+                    currentUrl = urlStack.peek();
+                    System.out.println(currentUrl);
+                } else {
+                    System.out.println("no previous URLs");
+                }
+            } else if (input.equals("forward")) {
+                if (forwardUrl.isEmpty()) {
+                    System.out.println("no next URLs");
+                } else {
+                    urlStack.push(forwardUrl.poll());
+                    System.out.println(urlStack.peek());
+                }
+            } else {
+                urlStack.push(input);
+                currentUrl = input;
+                forwardUrl.clear();
+                System.out.println(currentUrl);
+            }
+            input = scanner.nextLine();
+        }
+    }
+}
