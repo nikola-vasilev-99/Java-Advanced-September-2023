24a0f2dc4227670c4c1db368b12a04c569c079bd
Nikola Vasilev <nikola.vasilev99@icloud.com>
2023-09-15 14:16:44 +0300

    nomessage

--- src/Unit1_StacksAndQueues/EX3_DecimalToBinaryConverter.java
+++ src/Unit1_StacksAndQueues/EX3_DecimalToBinaryConverter.java
@@ -1,10 +1,23 @@
 package Unit1_StacksAndQueues;
 
+import java.util.ArrayDeque;
 import java.util.Scanner;
 
 public class EX3_DecimalToBinaryConverter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+        ArrayDeque <Integer> stack = new ArrayDeque<>();
+        
+        while (number >= 1) {
+            stack.push(number % 2);
+            number /= 2;
+        }
+
+        while (stack.size() > 0) {
+            System.out.print(stack.pop());
+        }
 
 
     }
