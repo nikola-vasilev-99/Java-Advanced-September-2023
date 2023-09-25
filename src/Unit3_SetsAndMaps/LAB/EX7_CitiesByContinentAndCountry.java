0a58d06c5c2ea116feb980248bc9b88ad0ddf85e
Nikola Vasilev <nikola.vasilev99@icloud.com>
2023-09-25 18:42:57 +0300

    -

--- src/Unit3_SetsAndMaps/LAB/EX7_CitiesByContinentAndCountry.java
+++ src/Unit3_SetsAndMaps/LAB/EX7_CitiesByContinentAndCountry.java
@@ -1,12 +1,33 @@
 package Unit3_SetsAndMaps.LAB;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class EX7_CitiesByContinentAndCountry {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        LinkedHashMap <String, LinkedHashMap<String, List<String>>> map = new LinkedHashMap<>();
         int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            String [] data = scanner.nextLine().split(" ");
+            String continent = data[0];
+            String country = data[1];
+            String city = data[2];
+            //key1 -> Continent
+            //key2 -> Country
+            map.putIfAbsent(continent, new LinkedHashMap<>());
+            map.get(continent).putIfAbsent(country, new ArrayList<>());
+            map.get(continent).get(country).add(city);
+
+        }
+        for (Map.Entry<String, LinkedHashMap<String, List<String>>> entry1 : map.entrySet()) {
+            System.out.println(entry1.getKey() + ":");
+            for (Map.Entry<String, List<String>> entry2 : entry1.getValue().entrySet()) {
+                System.out.print("  " + entry2.getKey() + " -> ");
+                System.out.println(String.join(", ", entry2.getValue()));
+            }
+        }
 
     }
 }
