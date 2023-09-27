e3a7b4678a3f0acfdd98e8c4c9a3203110207de0
Nikola Vasilev <nikola.vasilev99@icloud.com>
2023-09-27 12:11:11 +0300

    -

---
+++ src/Unit3_SetsAndMaps/EX/TestSortingMap_CheatSheet.java
@@ -0,0 +1,32 @@
+package Unit3_SetsAndMaps.EX;
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+import static java.util.Map.Entry.comparingByValue;
+
+public class TestSortingMap_CheatSheet {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Map<String, Integer> peopleMap = new LinkedHashMap<>();
+        peopleMap.put("nik", 45);
+        peopleMap.put("lea", 4);
+        peopleMap.put("zozo", 23);
+        peopleMap.put("dodo", 56);
+        peopleMap.put("mimi", 12);
+
+        //ascending order by value
+        System.out.println("ascending order by value");
+        peopleMap.entrySet().stream().sorted(comparingByValue())
+                .forEach(e -> System.out.println(e.getKey() + " is " + e.getValue()));
+
+        //descending order by value
+        System.out.println("descending order by value");
+        peopleMap.entrySet().stream().sorted(Collections.reverseOrder(comparingByValue()))
+                .forEach(e -> System.out.println(e.getKey() + " is " + e.getValue()));
+
+    }
+}
